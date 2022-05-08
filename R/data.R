a64b95540261f65e0cb7c070a8c7d0954f316f49
JustinThorp <jjthorp@sbcglobal.net>
2022-05-08 15:26:30 -0700

    Included the data set. Added data documentation and fixed some issues cuasing warnings in check()

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Observational Educational Dataset
+#'
+#' A simulated dataset containing the prices and other attributes of almost
+#' 54,000
+#' diamonds.
+#'
+#' @format A data frame with 1000 rows and 7 variables:
+#' \describe{
+#'   \item{SAT_MATH}{SAT Math Score}
+#'   \item{HSGPA}{High School GPA}
+#'   \item{AGE}{Age of Student}
+#'   \item{GENDER}{Gender of Student}
+#'   \item{URM}{Under Represented Minority}
+#'   \item{A}{Treatment Variable}
+#'   \item{Y}{Students Final Grade}
+#'   ...
+#' }
+#' @source \url{Wilke, Morten C., et al. “Estimating the Optimal Treatment
+#' Regime for Student Success Programs.” Behaviormetrika, vol. 48, no. 2, 2021,
+#' pp. 309–343., https://doi.org/10.1007/s41237-021-00140-0. }
+"educational"
